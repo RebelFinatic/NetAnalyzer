6004f0db3843a25d108b5ce1e828a1c4b69724b8
Thevindu Warnakulasuriya <13820898+RebelFinatic@users.noreply.github.com>
2024-06-29 12:23:40 +1000

    Check if ping is successful, if not then ICMP is disabled.

--- netanalyzer.py
+++ netanalyzer.py
@@ -3,6 +3,7 @@ import socket
 import ipaddress
 from ping3 import ping, verbose_ping
 
+# Gathers relevant information from an ip address
 def get_isp_from_ip(ip):
     try:
         response = requests.get(f'https://ipinfo.io/{ip}/json')
@@ -12,7 +13,14 @@ def get_isp_from_ip(ip):
         timezone = data.get('timezone', 'Timezone not found')
         country = data.get('country', 'Country not found')
         org = data.get('org', 'ISP not found')
-        latency = round(ping(ip) * 1000)
+        
+        # Performs latency test, if failed it assumes that ICMP is disabled
+        latency = ping(ip)
+        if latency is not None:
+            latency = round(latency * 1000)
+            latency = str(latency) + " ms"
+        else:
+            latency = 'ICMP may be disabled (Ping Test Failed)'
 
         print(f"\n--------IP DETAILS--------\n")
         print(f"IP: {ip}")
@@ -21,19 +29,27 @@ def get_isp_from_ip(ip):
         print(f"City: {city}")
         print(f"Timezone: {timezone}")
         print(f"Country: {country}")
-        print(f"Ping: {latency} ms")
+        print(f"Ping: {latency}")
         print(f"\n---------------------------")
 
     except requests.RequestException as e:
-        return f'Error: {e}'
+        print(f'Error: {e}')
+    except Exception as e:
+        print(f'Unexpected error: {e}')
     
+# Check if ip is valid if not assume its a domain
 def check(ip):
     try:
         ipaddress.ip_address(ip)
         get_isp_from_ip(ip)
     except ValueError:
-        domain_to_ip = socket.gethostbyname(ip)
-        get_isp_from_ip(domain_to_ip)
+        try:
+            domain_to_ip = socket.gethostbyname(ip)
+            get_isp_from_ip(domain_to_ip)
+        except socket.gaierror as e:
+            print(f'Error resolving domain: {e}')
+    except Exception as e:
+        print(f'Unexpected error: {e}')
 
 ip_or_domain = input("Please enter an IP or Domain: ")
 check(ip_or_domain)
